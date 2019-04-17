ba47b43e9789889cf47224f8d1dfe6512f311f0d
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-04-17 09:59:18 -0700

    wip

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -1,10 +1,6 @@
 module.exports = function(controller) {
 
-  var acronyms = [
-    {key: "SWG", value: "Secure Web Gateway\n" +
-          "A Secure Web gateway is a solution that filters unwanted software/malware from user-initiated Web/Internet traffic and enforces corporate and regulatory policy compliance.\n"},
-    {key: 2, value: 20},
-    {key: 3, value: 30}];
+  var acronyms = {"SWG":"WHAAAA"};
 
 
   controller.on('bot_space_join', function(bot, message) {
@@ -14,7 +10,7 @@ module.exports = function(controller) {
   });
 
   controller.hears(acronyms.keys(), 'direct_message,direct_mention', function(bot, message) {
-    bot.reply(message, acronyms.values());
+    bot.reply(message, acro);
   });
 
   controller.on('user_space_join', function(bot, message) {
