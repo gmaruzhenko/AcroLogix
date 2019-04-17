adf9ad4cfc57717f9017193fc724fa65e6902a5c
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-04-17 09:49:11 -0700

    added map and call

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -1,5 +1,11 @@
 module.exports = function(controller) {
 
+  var acronyms = [
+    {key: "SWG", value: "Secure Web Gateway\n" +
+          "A Secure Web gateway is a solution that filters unwanted software/malware from user-initiated Web/Internet traffic and enforces corporate and regulatory policy compliance.\n"},
+    {key: 2, value: 20},
+    {key: 3, value: 30}];
+
 
   controller.on('bot_space_join', function(bot, message) {
 
@@ -7,8 +13,8 @@ module.exports = function(controller) {
 
   });
 
-  controller.hears('hello', 'direct_message,direct_mention', function(bot, message) {
-    bot.reply(message, 'WAAAZZZZUUUUPP');
+  controller.hears(acronyms.keys(), 'direct_message,direct_mention', function(bot, message) {
+    bot.reply(message, acronyms.values());
   });
 
   controller.on('user_space_join', function(bot, message) {
