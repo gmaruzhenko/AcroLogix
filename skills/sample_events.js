c477d697fb713ed5c897c175bcb86c9c668a126c
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-04-17 10:03:09 -0700

    wip

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -1,6 +1,8 @@
 module.exports = function(controller) {
 
-  var acronyms = {"SWG":"WHAAAA"};
+  var acronyms_map = {"SWG":"WHAAAA"};
+  var acronyms = ["SWG"]
+
 
 
   controller.on('bot_space_join', function(bot, message) {
@@ -9,8 +11,8 @@ module.exports = function(controller) {
 
   });
 
-  controller.hears(acronyms.keys(), 'direct_message,direct_mention', function(bot, message) {
-    bot.reply(message, acronyms.value(message));
+  controller.hears(acronyms.values(), 'direct_message,direct_mention', function(bot, message) {
+    bot.reply(message, acronyms_map.value(message));
   });
 
   controller.on('user_space_join', function(bot, message) {
